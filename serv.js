614d81e9e1cce2483b59d0a6ce8e8c9a0aae910d
Mark Kopenga <mkopenga@gmail.com>
2017-07-16 16:20:30 +0200

    worked on serverside clans request

--- serv.js
+++ serv.js
@@ -9,6 +9,11 @@ var base64 = require('base64-js');
 var pics = require('pics');
 var ejs = require('ejs');
 var promptly = require('promptly');
+var fetch = require('node-fetch');
+
+function listtorurl(arrr,begin,end) {
+  return (JSON.stringify(arrr.slice(begin, end)).replace('[', '').replace(']', '').replace(/,/g, '%2C'))
+}
 
 var app = express();
 app.set('json spaces', 2);
@@ -32,3 +37,36 @@ app.get('/', function(req, res) {
 app.get('*', function(req, res){
   res.status(404).sendFile(__dirname + '/www/404.html');
 });
+
+function clanstolist() {
+  var list = [];
+  function clans(nr) {
+    request('https://api.worldoftanks.eu/wgn/clans/list/?application_id=f53bdb585a307dbb8e5c1fdcbf213384&limit=100&game=wot&fields=clan_id&page_no=' + nr, function (error, response, body) {
+      body = JSON.parse(body);
+      if (body.status == 'ok') {
+        var convert = [];
+        for (var i = 0; i < body.data.length; i++) {
+          convert.push(body.data[i].clan_id)
+        }
+        fetch('https://api.worldoftanks.eu/wgn/clans/info/?application_id=f53bdb585a307dbb8e5c1fdcbf213384&clan_id=' + listtorurl(convert, 0, 100) + '&fields=description')
+          .then(function(res) {
+            return res.text();
+          }).then(function(body) {
+            body = JSON.parse(body)
+            console.log(body);
+          });
+        // for (var i = 0; i < body.data.length; i++) {
+        //   arr.push(body.data[i].clan_id)
+        // }
+        // list.push(arr);
+        // console.log(list.length);
+        // clans(nr + 1);
+      } else {
+        var total = list.length * 100;
+        console.log('all clans collected: ' + total + ' clans id\'s');
+      }
+    });
+  }
+  clans(1)
+}
+clanstolist();
