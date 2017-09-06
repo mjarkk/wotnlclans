28d3d9a5af6683f3abfbefa179ed9a3c9ff304e7
Mark Kopenga <mkopenga@gmail.com>
2017-09-06 14:51:41 +0200

    updated updater

--- update/update.sh
+++ update/update.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-echo "Start backup"
+echo "Start backup ..."
 
-mkdir ../wotnlclans-backup
-cp -r ./wotnlclans/db/clanmedia ./wotnlclans-backup/clanmedia
-cp -r ./wotnlclans/db/claninf ./wotnlclans-backup/claninf
-cp -r ./wotnlclans/db/clanreports ./wotnlclans-backup/clanreports
-cp ./wotnlclans/db/dev.json ./wotnlclans-backup/dev.json
-cp ./wotnlclans/db/blockedclans.json ./wotnlclans-backup/blockedclans.json
+cd ..
+mkdir wotnlclans-backup
+rm -rf ./wotnlclans/node_modules
+cp -rf ./wotnlclans ./wotnlclans-backup
 
 echo "dune making a backup"
+echo "Installing new version ..."
+
+rm -rf ./wotnlclans
+git clone https://github.com/mjarkk/wotnlclans
+
+
+echo "dune installing new version"
+echo "Copying old files"
+
+rm -rf ./wotnlclans/db/clanmedia
+rm -rf ./wotnlclans/db/claninf
+rm -rf ./wotnlclans/db/clanreports
+mkdir ./wotnlclans/db/clanmedia
+mkdir ./wotnlclans/db/claninf
+mkdir ./wotnlclans/db/clanreports
+cp -rf ./wotnlclans-backup/db/clanmedia ./wotnlclans/clanmedia
+cp -rf ./wotnlclans-backup/db/claninf ./wotnlclans/claninf
+cp -rf ./wotnlclans-backup/db/clanreports ./wotnlclans/clanreports
+cp ./wotnlclans-backup/db/dev.json ./wotnlclans/dev.json
+cp ./wotnlclans-backup/db/blockedclans.json ./wotnlclans/blockedclans.json
+
+echo "Dune Copying old files"
+echo "Installing npm packages"
+
+cd wotnlclans
+npm i
+
+echo "Dune installing packages"
