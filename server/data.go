d7362285df1dada54ef6bdc8b0193d02a2dbf5fb
mjarkk <mkopenga@gmail.com>
2018-12-29 16:02:29 +0100

    A try to fix a server issue

--- server/data.go
+++ server/data.go
@@ -9,8 +9,10 @@ import (
 func serveDataRoutes(r *gin.Engine) {
 	r.GET("/clanData", func(c *gin.Context) {
 		data, err := db.GetCurrentClansData()
+		c.Request.Close = true
 		if err != nil {
 			c.JSON(400, nil)
+			return
 		}
 		c.JSON(200, data)
 	})
