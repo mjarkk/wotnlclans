4d9b9d17b6a78b92d2d972a3f5ab52ee49fa3c9e
mjarkk <mkopenga@gmail.com>
2019-01-14 15:31:06 +0100

    Added the webp location to the icon

--- server/static.go
+++ server/static.go
@@ -9,5 +9,6 @@ func serveStaticFiles(r *gin.Engine) {
 	r.StaticFile("/", "./web_static/build/index.html")
 	r.StaticFile("/icons/json", "./icons/allIcons.json")
 	r.StaticFile("/icons/png", "./icons/allIcons.png")
+	r.StaticFile("/icons/webp", "./icons/allIcons.webp")
 	r.Static("/js", "./web_static/build/js")
 }
