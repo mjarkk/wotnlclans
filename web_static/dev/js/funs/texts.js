33398cf09c763b7dad632b50ef85a0263b9b8c77
mjarkk <mkopenga@gmail.com>
2019-01-23 13:20:33 +0100

    Changed some dutch words to english

--- web_static/dev/js/funs/texts.js
+++ web_static/dev/js/funs/texts.js
@@ -2,6 +2,6 @@ export const discordBot = `# How to use the discord bot
 This discord bot can show the stats of clans  
 Type \`w help\` to get started.  
   
-Heb je nieuwe gaafe ideeen?  
-Neen dan contact op met: [info@dpg-hq.eu](mailto:info@dpg-hq.eu)  
+Do you have a good idea?  
+Please contact: [info@dpg-hq.eu](mailto:info@dpg-hq.eu)  
 `
